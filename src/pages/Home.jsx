b435465520b66f34121213d8cc25ecf748ddc354
IonutBelean <ionutbelean4@gmail.com>
2022-09-04 19:43:34 +0300

    carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Container, Card, Button } from "react-bootstrap";
+import { Container, Card, Button, Carousel } from "react-bootstrap";
 import Layout from "../components/Layout";
 import HomeCSS from "./Home.module.css";
 
@@ -19,6 +19,149 @@ const Home = () => {
             <Button>See Designs</Button>
           </Card.ImgOverlay>
         </Card>
+      </Container>
+      <Container className="p-0">
+        <Carousel className={`${HomeCSS.carousel_container} bg-dark`}>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/1.jpg")}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/2.jpg")}
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/3.jpg")}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/4.jpg")}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/5.jpg")}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/6.jpg")}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/7.jpg")}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/8.jpg")}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/9.jpg")}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/10.jpg")}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className={`${HomeCSS.carousel} d-block w-100`}
+              src={require("../pics/Carousel/11.jpg")}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </Container>
     </Layout>
   );
